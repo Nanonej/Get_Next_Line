0fbe0c40c58aaea308f215ff1f0207d5ed2a0a9d
Antonin RIDOLFI <aridolfi@student.42.fr>
2016-11-30 10:58:38 +0100

    delete ft_remalloc

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,11 @@
 /*   By: aridolfi <aridolfi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 22:17:55 by aridolfi          #+#    #+#             */
-/*   Updated: 2016/11/30 10:41:09 by aridolfi         ###   ########.fr       */
+/*   Updated: 2016/11/30 10:57:25 by aridolfi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	ft_remalloc(static char	*save, int n)
-{
-	if (!(save = (char*)malloc(sizeof(char) * ft_strlen(save))))
-}
 
 int		get_next_line(const int fd, char **line)
 {
@@ -24,6 +19,8 @@ int		get_next_line(const int fd, char **line)
 	static char		*save;
 
 	ret = 1;
+	if (!(save = (char*)malloc(sizeof(char) * (ft_strlen(save) + BUFF_SIZE))))
+		return (ERROR);
 	ret = read(fd, buff, BUFF_SIZE);
 	buff[BUFF_SIZE + 1] = '\0';
 	line = ft_strsplit(buff, '\n');
